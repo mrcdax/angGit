059be243cbf9dfe479d01877cc85151935aca4ac
mrcdax <mrcdax@outlook.com>
2014-07-16 18:13:40 +0300

    Added form with ajax call and php response.

---
+++ ajax/process.php
@@ -0,0 +1,21 @@
+<div>Result:<br/>
+  <?php
+if (!empty($_POST))
+{
+ $name = $_POST['fname'];
+ echo "Welcome <b>". $name. "</b>";
+ $age = $_POST['age'];
+ echo "<br />Your age is: <b>". $age. "</b>";
+ $func = $_POST['func'];
+ echo "<br/> You are : <b>".$func. "</b>";
+ $awesome = $_POST['awesome'];
+ echo "<br/>Are you awesome? <b>". $awesome. "</b>";
+ if(isset($_POST['hobby'])){
+     echo "<br/> You like: ";
+     foreach($_POST['hobby'] as $hobby){
+        echo "<b>".$hobby. " </b>";
+     }
+  }
+}
+?>
+</div>
---
+++ ajax/script.js
@@ -0,0 +1,42 @@
+﻿$(function(){
+    $("#btn").click(function (e) {
+        var data = $("#myForm").serialize();
+        console.log("Here: " + data);
+        $.ajax({
+            url: "process.php",
+            type: "POST",
+            data: data,
+            cache: false
+        }).done(function (result) {
+            $("#result").html(result);
+        })
+        e.preventDefault();
+    });
+
+    /* 
+    
+    //javascript oldschool
+    document.getElementById("requestJS").addEventListener('click', function () {
+        httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function () {
+            if (httpRequest.readyState === 4 && httpRequest.status === 200) {//response 4 (complete) and 200(ok)
+                alert(httpRequest.responseText);
+            }
+        };
+        httpRequest.open('GET', 'test.html?123', true);
+        httpRequest.send();
+    }, false);
+
+    //jquery style
+    $("#requestJQ").click(function () {
+        $.ajax({ url: 'test.html', cache: false })
+            .done(function (result) {
+                alert(result);
+            })
+            .fail(function () {
+                alert("fail");
+            });
+    });
+
+    */
+});
