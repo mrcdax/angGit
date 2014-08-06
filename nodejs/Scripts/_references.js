0a8e8f8347c3483bf850f1f6fd61aaa09c58a4c2
mrcdax <mrcdax@outlook.com>
2014-08-06 14:07:46 +0300

    Added pouchdb notepad project

--- nodejs/Scripts/_references.js
+++ nodejs/Scripts/_references.js
@@ -1,3 +1,6 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../script.js" />
+/// <reference path="../express.js" />
+/// <reference path="../nodeserver.js" />
+/// <reference path="../processForm.js" />
 /// <reference path="jquery-2.1.1.js" />
